142f52b8274b4284c1594b14a8a34add46fcd780
PieterT2000 <info@codebirds.nl>
2022-01-20 20:05:51 +0100

    STREET-4173: add delete/list worklog functionality

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,17 @@
 #!/usr/local/bin/node
 import { config } from "dotenv";
-import { Command } from "commander/esm.mjs";
+import { Argument, Command } from "commander/esm.mjs";
 import * as readline from "node:readline";
 import { stdin as input, stdout as output } from "node:process";
-import { convert } from "html-to-text";
 import db from "../lib/db.js";
 import JiraClient from "../lib/jira-client/lib/index.js";
 import path from "path";
-import { getDirname } from "../lib/utils.js";
-import Output from "./../lib/console-output.js";
+import { formatToJiraApiDateString, getDirname } from "../lib/utils/utils.js";
+import Output from "../lib/utils/console-output.js";
+import cliConfig from "../cli-config.js";
+import { checkFetchResult } from "../lib/utils/errorHandling.js";
+import getIssue from "../lib/actionHandlers/getIssue.js";
+import { deleteWorkLog, listWorkLogs } from "../lib/actionHandlers/worklogs.js";
 
 const __dirname = getDirname(import.meta.url);
 config({ path: path.join(__dirname, "..", ".env") });
@@ -18,9 +21,8 @@ const jiraConfig = {
   username: process.env.JIRA_USERNAME,
   apikey: process.env.JIRA_API_KEY,
 };
-const jc = new JiraClient(jiraConfig);
 
-const rl = readline.createInterface({ input, output });
+export const api = new JiraClient(jiraConfig);
 
 const program = new Command();
 
@@ -29,24 +31,42 @@ const commentsRequired = false;
 program
   .command("start")
   .description("Starts the timer")
-  .action(() => {
-    db.saveStartTime();
-    rl.close();
+  .action(db.saveStartTime);
+program
+  .command("log")
+  .addArgument(
+    new Argument("<type>", "The type of work/activity you want to log").choices(
+      Object.keys(cliConfig)
+    )
+  )
+  .description("Logs work on the activity specified in cli-config.js")
+  .action(async (type) => {
+    if (type in cliConfig) {
+      const { issueKey, comment, timeSpent } = cliConfig[type];
+      const started = formatToJiraApiDateString(new Date());
+      const resolved = checkFetchResult(
+        await api.addWorklog(issueKey, comment, started, timeSpent)
+      );
+      if (resolved) {
+        Output.success(`${comment} log added!`);
+      }
+    }
+    process.exit(0);
   });
+
 program
   .command("stop")
   .argument("[comment]")
   .description("Stops the timer")
   .action(async (comment) => {
     if (!comment && commentsRequired) {
+      const rl = readline.createInterface({ input, output });
       rl.question("Comment: ", (comment) => {
-        // Push to api and delete record
-        db.saveToApi(jc, comment);
+        db.saveToApi(comment);
         rl.close();
       });
     }
-    await db.saveToApi(jc, comment);
-    process.exit(0);
+    await db.saveToApi(comment);
   });
 
 program
@@ -54,15 +74,16 @@ program
   .argument("<issueKey>")
   .argument("[fields...]")
   .description("Finds that Jira issue with the key")
-  .action(async (issueKey, fields) => {
-    const result = await jc.findIssue(issueKey, fields);
-    if (result.errorMessages) {
-      result.errorMessages.map((e) => Output.error(e));
-    } else {
-      const htmlDescription = result.issues[0].renderedFields.description;
-      const plainTextDescription = convert(htmlDescription, { wordwrap: 80 });
-      console.log(plainTextDescription);
-    }
-    process.exit(0);
-  });
+  .action(getIssue);
+
+const worklog = program.command("worklog").alias("wl");
+worklog
+  .command("list")
+  .argument("[maxLen]", "Maximum number of worklogs you want to see")
+  .action(listWorkLogs);
+worklog
+  .command("del")
+  .argument("[worklogId]", "Id of the worklog you want to delete")
+  .action(deleteWorkLog);
+
 program.parse(process.argv);
